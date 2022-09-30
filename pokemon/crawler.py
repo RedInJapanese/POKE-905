e40f52d8d6acc7d345a2e81dee3b832c40c9bd87
RedInJapanese <abasu924@gmail.com>
2022-09-29 20:34:15 -0700

    comb through sprite database

--- pokemon/crawler.py
+++ pokemon/crawler.py
@@ -6,11 +6,15 @@ url = 'https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/r
 list = open('list.txt')
 buffer = ''
 count = 0
+test = ''
 
 for i in list: 
     buffer = str(count)
     buffer+='.png'
-    url+=i
+    url+=str(i)
+    url+='.png'
+    print(url)
+    url = 'https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/'
     lol = requests.get(url)
     open(buffer, "wb").write(lol.content)
     count+=1
