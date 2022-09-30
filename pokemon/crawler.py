6d35aa42de5d5de2f39338154fe66e25ee9c0532
RedInJapanese <abasu924@gmail.com>
2022-09-29 20:21:28 -0700

    test web crawler

--- pokemon/crawler.py
+++ pokemon/crawler.py
@@ -1,6 +1,6 @@
 import bs4
 import requests
 
-url = 'https://github.com/msikma/pokesprite/blob/master/icons/pokemon/regular/abomasnow-mega.png'
+url = 'https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular/abomasnow-mega.png'
 lol = requests.get(url)
 open('lol.png', "wb").write(lol.content)
