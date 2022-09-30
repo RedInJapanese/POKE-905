b5ac29677899a30b86b89ee675e5b4ef18350c95
RedInJapanese <abasu924@gmail.com>
2022-09-29 20:29:20 -0700

    comb through sprite database

--- pokemon/crawler.py
+++ pokemon/crawler.py
@@ -1,6 +1,16 @@
 import bs4
 import requests
 
-url = 'https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular/abomasnow-mega.png'
-lol = requests.get(url)
-open('lol.png', "wb").write(lol.content)
+url = 'https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/'
+
+list = open('list.txt')
+buffer = ''
+count = 0
+
+for i in list: 
+    buffer = str(count)
+    buffer+='.png'
+    url+=i
+    lol = requests.get(url)
+    open(buffer, "wb").write(lol.content)
+    count+=1
