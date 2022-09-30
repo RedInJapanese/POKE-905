07a5ff7490ef5be50efadf41596fbfe538991fcb
RedInJapanese <abasu924@gmail.com>
2022-09-30 10:23:19 -0700

    add test file to read lines

---
+++ test.rs
@@ -0,0 +1,10 @@
+use std::io::Read;
+use std::fs::File;
+
+fn main(){
+    let mut file = File::open("rgb.txt").expect("Error. Unable to open file.");
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).expect("Error. Unable to read file.");
+    println!("Contents: {}", contents)
+}
